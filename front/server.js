269bc396aff9ef7d1f69f7053ba6c2289280a04e
famgorag <famgorag@gmail.com>
2020-04-16 22:27:22 +0200

    add

--- front/server.js
+++ front/server.js
@@ -1,19 +1,10 @@
-var express         = require('express');
-var path            = require('path'); // модуль для парсинга пути
-var app = express();
-
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-
-app.use(app.router); // модуль для простого задания обработчиков путей
-
-
-
-app.get('/api', function (req, res) {
-    res.send('API is running');
-});
-
-app.listen(1337, function(){
-    console.log('Express server listening on port 1337');
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const port = 8000;
+app.use(bodyParser.urlencoded({ extended: true }));
+require('./routes')(app, {});
+app.listen(port, () => {
+    console.log('We are live on ' + port);
 });
